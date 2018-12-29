60b8296f020a6e0ca4c32f4129adbc529deaf8d6
Tereza Holm <holm.tereza@gmail.com>
2018-12-29 18:38:04 +0100

    Task complete

---
+++ src/se/terhol/pisemka21/LockerException.java
@@ -0,0 +1,14 @@
+package se.terhol.pisemka21;
+
+/**
+ * @author Tereza Holm
+ */
+public class LockerException extends Throwable {
+    public LockerException() {
+
+    }
+
+    public LockerException(String message) {
+        System.out.println(message);
+    }
+}
